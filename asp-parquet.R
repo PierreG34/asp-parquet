983b756bde58dfaeeef086088d9639dd71d887b2
Pierre <pierre.girard34@gmail.com>
2024-07-01 07:36:41 +0200

    add read and write function with parquet file

--- asp-parquet.R
+++ asp-parquet.R
@@ -10,22 +10,36 @@ outputdir = paste0(stringr::str_sub(getwd(),1,n),"/data")
 datatype = c("apt", "cie", "lsn")
 subdir = paste0(outputdir,"/asp-", datatype)
 
+#specify data path of each of the 3 files : apt, cie and lsn
+datapath_apt = paste0(outputdir,"/asp-apt.parquet")
+datapath_cie = paste0(outputdir,"/asp-cie.parquet")
+datapath_lsn = paste0(outputdir,"/asp-lsn.parquet")
+
 #functions----
-write_allcsv_from_dir_to_singleparquetfile = function(directory)
-{
+read_parquet_asp = function(asp_type = "apt"){
+  datapath = paste0(outputdir,"/asp-",asp_type,".parquet")
+  tmp = read_parquet(datapath)
+  return(tmp)
+}
+
+write_allcsv_from_dir_to_singleparquetfile = function(directory){
   f = list.files(path = directory, pattern = "csv\\b", all.files = TRUE, full.names = TRUE, recursive = TRUE, ignore.case=T) #select files with names ending in "csv"
   df = NULL
   for (datapath in f){
     tmp = read.csv2(datapath)
     df = rbind(df,tmp)
   }#row bind all files
-  
   datapath = paste0(directory,".parquet")
   write_parquet(
     x = df,
     sink = datapath
   )#write to WD parquet file with name of directory
 }
+
+
+
+#read file----
+df = read_parquet_asp("lsn")
 
 #write csv files from dir to single parquet file----
 for (i in 1:3){
